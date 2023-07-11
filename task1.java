049f7b98f5b4ad43997cf8715b08602d3d0b47d6
IRINAG <ira848@mail.ru>
2023-07-12 00:20:34 +0600

    Add sum

--- task1.java
+++ task1.java
@@ -4,6 +4,10 @@
 public class task1 {
 
     public static void main(String[] args) {
+        int a = 34;
+        int b = 67;
+        int sum = a + b;
+        System.out.println(sum);
         
     }
 }
